0e8f54ba8116b3a59be7522f4631cbaf01adff6e
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-25 18:28:54 -0500

    Custom Writer

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,10 +16,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	// create a slice of type byte with 99999 empty elements.
-	// this is because read() cannot resize []byte if it becomes full.
-	resp.Body.Read(bs)
+	// bs := make([]byte, 99999)
+	// // create a slice of type byte with 99999 empty elements.
+	// // this is because read() cannot resize []byte if it becomes full.
+	// resp.Body.Read(bs)
 
+	// fmt.Println(string(bs))
+
+	// io.Copy(os.Stdout, resp.Body)
+	// // os.Stdout implements the Writer interface
+	// // resp.Body implements the Reader interface
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Finished writing", len(bs), "bytes")
+
+	return len(bs), nil
 }
